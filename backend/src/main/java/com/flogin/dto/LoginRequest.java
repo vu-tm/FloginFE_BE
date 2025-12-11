2b005b40b40148440f0a10be8c85e0549977c957
Tuan Khoi <9tuankhoi6@gmail.com>
2025-12-11 09:44:52 +0700

    Bỏ valid LoginRequest vì đã valid trong authService

--- backend/src/main/java/com/flogin/dto/LoginRequest.java
+++ backend/src/main/java/com/flogin/dto/LoginRequest.java
@@ -1,18 +1,8 @@
 package com.flogin.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-
 public class LoginRequest {
-    @NotBlank(message = "Username không được để trống")
-    @Size(min = 3, max = 50, message = "Username phải từ 3 đến 50 ký tự")
-    @Pattern(regexp = "^[a-zA-Z0-9\\-._]+$", message = "Username chỉ được chứa chữ cái, số, dấu gạch ngang, dấu chấm và gạch dưới")
-    private String username;
 
-    @NotBlank(message = "Password không được để trống")
-    @Size(min = 6, max = 100, message = "Password phải từ 6 đến 100 ký tự")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+{}\\[\\]:;<>,.?~\\\\/-])[\\S]+$", message = "Password phải chứa ít nhất 1 chữ hoa, 1 chữ thường, 1 số và 1 ký tự đặc biệt, không chứa khoảng trắng")
+    private String username;
     private String password;
 
     public LoginRequest(String username, String password) {
